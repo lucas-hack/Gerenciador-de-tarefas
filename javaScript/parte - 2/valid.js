a657beeb23021fa6273f277de0200fd4045dbb1b
lucas-hack <130858096+lucas-hack@users.noreply.github.com>
2023-05-18 12:56:40 -0300

    paginas 1 e 2
    pagina 1 e 2 concluída, precisa de alguns detalhes pequenos

--- javaScript/parte - 2/valid.js
+++ javaScript/parte - 2/valid.js
@@ -3,19 +3,19 @@
 let email = document.getElementById("email");
 let password = document.getElementById("password");
 let form = document.querySelector("form");
-let textForm = document.getElementById("textForm");
-let textEmail = document.getElementById("textEmail");
-let textPassword = document.getElementById("textPassword");
+// let textForm = document.getElementById("textForm");
+// let textEmail = document.getElementById("textEmail");
+// let textPassword = document.getElementById("textPassword");
 
 form.addEventListener('submit', (e) => {
     if (email.value == "" || password.value == "") {
-        textForm.textContent = "Você precisa preencher os campos necessários";
+        // textForm.textContent = "Você precisa preencher os campos necessários";
     } else if (validEmail(email.value) === true && validPassword(password.value) === true){
         console.log(email.value);
         console.log(password.value);
-        textForm.textContent = "";
-        textEmail.textContent = "";
-        textPassword.textContent = "";
+        // textForm.textContent = "";
+        // textEmail.textContent = "";
+        // textPassword.textContent = "";
     } else {
         console.log("requisição não atendida")
     }
@@ -25,17 +25,19 @@ form.addEventListener('submit', (e) => {
 
 email.addEventListener("keyup", () => {
     if (validEmail(email.value) !== true) {
-        textEmail.textContent = "o formato do email deve ser @gmail.com";
+        // textEmail.textContent = "o formato do email deve ser @gmail.com";
     } else {
-        textEmail.textContent = "";
+        console.log("email aceito")
+        // textEmail.textContent = "";
     }
 })
 
 password.addEventListener("keyup", () => {
     if (validPassword(password.value) !== true){
-        textPassword.textContent = "A senha precisa ter 6 digitos, no mínimo um maiúsculo e um minúsculos"
+        // textPassword.textContent = "A senha precisa ter 6 digitos, no mínimo um maiúsculo e um minúsculos"
     } else {
-        textPassword.textContent = ""
+        console.log("senha aceita")
+        // textPassword.textContent = ""
     }
 })
 
@@ -45,6 +47,7 @@ function validEmail(email) {
 }
 
 function validPassword(password) {
-let passwordPattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#])(?:([0-9a-zA-Z$*&@#])(?!\1)){8,}$/;
+let passwordPattern = (/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])([a-zA-Z0-9]{6,})$/)
+// /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#])(?:([0-9a-zA-Z$*&@#])(?!\1)){8,}$/
 return passwordPattern.test(password)
 }
